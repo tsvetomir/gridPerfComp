e86ee471f8dedec87b46d00735be61becdf10187
Konstantin Dinev <kdinev@mail.bw.edu>
2018-09-18 11:38:29 +0300

    Updating some packages and the column defs.

--- src/app/igxgrid-base/igxgrid-base.component.ts
+++ src/app/igxgrid-base/igxgrid-base.component.ts
@@ -8,6 +8,27 @@ import { Observable } from 'rxjs';
   styleUrls: ['./igxgrid-base.component.css']
 })
 export class IgxGridBaseComponent implements OnInit {
+  public columnDefs = [
+    {headerName: 'ID', field: 'ID', width: 60},
+    {headerName: 'Category', field: 'Category', width: 120},
+    {headerName: 'Type', field: 'Type', width: 100},
+    {headerName: 'Spread', field: 'Spread', width: 90},
+    {headerName: 'Open Price', field: 'OpenPrice', width: 90},
+    {headerName: 'Price', field: 'Price', width: 90},
+    {headerName: 'Buy', field: 'Buy', width: 90},
+    {headerName: 'Sell', field: 'Sell', width: 90},
+    {headerName: 'Change', field: 'Change', width: 90},
+    {headerName: 'Change(%)', field: 'ChangePercent'},
+    {headerName: 'Volume', field: 'Volume'},
+    {headerName: 'High(D)', field: 'HighD'},
+    {headerName: 'Low(D)', field: 'LowD'},
+    {headerName: 'High(Y)', field: 'HighY'},
+    {headerName: 'Low(Y)', field: 'LowY'},
+    {headerName: 'Start(Y)', field: 'StartY'},
+    {headerName: 'Low(Y)', field: 'LowY'},
+    {headerName: 'Spread', field: 'Spread'}
+  ];
+
   public data: Observable<any[]>;
 
   constructor(private finDataService: DataGenService) {
